3b794ceb9632ebabac9c8fdbda8c59d1546d8723
SASBD <sofianesouidipro@gmail.com>
2025-05-14 21:07:08 +0200

    Ajouts des commentaires et structure du code

--- src/Djikstra/Dijkstra.java
+++ src/Djikstra/Dijkstra.java
@@ -8,25 +8,39 @@ public class Dijkstra<T> implements ShortestPath<T> {
 
 	@Override
 	public Distances<T> compute(Graph<T> g, T src, Animator<T> animator) throws IllegalArgumentException {
+		// initialisation des donnees
+		// Distance : (sommet,distance min)
+		// Predecesseur : (sommet, sommet)
+		// Vus : liste unique des sommets
 		Map<T, Integer> distance = new HashMap<>();
 		Map<T, T> pred = new HashMap<>();
 		Set<T> vus = new HashSet<>();
 
+		// iniatialisation de la queue, trie en ordre croissant les distances
 		PriorityQueue<T> file = new PriorityQueue<>(Comparator.comparingInt(distance::get));
 
+		// initialisation des valeurs pour le sommet de depart
+		// pas de predecesseur et distance a 0 (lui meme)
+		// premier dans la queue
 		distance.put(src, 0);
 		pred.put(src, null);
 		file.add(src);
 
+		// Boucle de l'algo : traite tout les sommets non visites
 		while (!file.isEmpty()) {
+			// retourne le sommet avec l'arc la dist la plus petite
 			T courant = file.poll();
 
 			if (vus.contains(courant)) continue;
 			vus.add(courant);
-
+			
+			animator.accept(courant, distance.get(courant));
 
 			List<Graph.Arc<T>> successeurs = g.getSucc(courant);
+
+			// boucle qui calcule les distances des successeurs
 			for (int i = 0; i < successeurs.size(); i++) {
+				// extractiom des valeurs des sommets voisins dans la boucle
 				Graph.Arc<T> arc = successeurs.get(i);
 				T voisin = arc.dst();
 				int poids = arc.val();
@@ -37,10 +51,11 @@ public class Dijkstra<T> implements ShortestPath<T> {
 
 				int nouvelleDistance = distance.get(courant) + poids;
 
+				// MAJ de la distance/pred du sommet voisin si plus court ou nouveau
 				if (!distance.containsKey(voisin) || nouvelleDistance < distance.get(voisin)) {
 					distance.put(voisin, nouvelleDistance);
 					pred.put(voisin, courant);
-					file.add(voisin);
+					file.add(voisin); // On met a jour la queue
 				}
 			}
 		}
