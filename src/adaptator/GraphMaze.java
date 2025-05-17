39776e4578b650a5b60b171a3dd5b1c2bdc94fbc
SASBD <sofianesouidipro@gmail.com>
2025-05-17 12:08:30 +0200

    Ajout de commentaires

--- src/adaptator/GraphMaze.java
+++ src/adaptator/GraphMaze.java
@@ -9,11 +9,13 @@ import java.util.List;
 public class GraphMaze<C> implements Graph<C> {
 
     private final Maze<C> maze;
-
+    // on definit le labyrinthe Maze avec les cellules C
     public GraphMaze(Maze<C> maze) {
         this.maze = maze;
     }
 
+    // On cree des arcs avec les voisins sans murs de poids 1.
+    // On calcule donc le chemin le plus court dans un labyrinthe avec Dijkstra
     @Override
     public List<Graph.Arc<C>> getSucc(C sommet) {
         List<Graph.Arc<C>> succ = new ArrayList<>();
--- src/graph/GraphMethod.java
+++ src/graph/GraphMethod.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class GraphMethod<T> implements graph.Graph<T>{
 
+    // Hashmap (sommet, list d'arc)
     protected final Map<T, List<Graph.Arc<T>>> adj = new HashMap<>();
 
     @Override
